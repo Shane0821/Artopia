a5c7627892a4f1d1cf979b758fdfc4e50f17e47d
Shane0821 <jpanav@connect.ust.hk>
2023-10-22 21:52:25 +0800

    update config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,11 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
     },
   },
   plugins: [],
