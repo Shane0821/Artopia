5a6122acb2c1672644c6f4982a16ce043f14f75a
Shane0821 <jpanav@connect.ust.hk>
2023-10-24 12:54:21 +0800

    upd next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        // domains: ['lh3.googleusercontent.com']
+    },
+    webpack(config) {
+    config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+    }
+    return config
+    }
+}
 
 module.exports = nextConfig
