e81ba6ac113cfe72a2ccdeef0d34a2b556b4e75d
Shane0821 <jpanav@connect.ust.hk>
2023-10-25 12:19:28 +0800

    upd nav bar

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -11,52 +11,43 @@ import type { SearchProps } from 'antd';
 const { Search } = Input;
 
 function Nav() {
-  const loggedIn = false;
+  const loggedIn = true;
 
   const onSearch: SearchProps['onSearch'] = (value: any, _e: any, info: any) => { console.log(info?.source, value) };
 
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-      <div className='flex gap-3 md:gap-5'>
+    <nav className='nav' style={{background: '#fafafa'}}>
+      <div className='flex gap-3 md:gap-5 flex-center'>
         <Link href='/' className='flex gap-2 flex-center'>
           <Image
             src='/assets/images/logo.svg'
             alt='Artopia logo'
-            width={30}
-            height={30}
+            width={35}
+            height={35}
             className='object-contain'
           />
           <p className='logo_text'>Artopia</p>
         </Link>
-        <Link href='/collection' className='flex-center'>Collection</Link>
-        <Link href='/autction' className='flex-center'>Auction</Link>
-        <Link href='/create' className='flex-center'>Create</Link>
+        <Link href='/collection' className='nav_link'>Collection</Link>
+        <Link href='/autction' className='nav_link'>Auction</Link>
+        <Search className='flex-center' placeholder="input search text" onSearch={onSearch} style={{ width: 300 }} allowClear/>
       </div>
 
-      <div className='flex'>
-        <Search placeholder="input search text" onSearch={onSearch} style={{ width: 350 }} allowClear/>
-      </div>
-
-      <div className='flex'>
+      <div className='flex gap-3 md:gap-5 flex-center'>
         {loggedIn ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link href='/create-prompt' className='black_btn'>
-              Create Artwork
+            <Link href='/create' className='nav_link'>
+              Create
             </Link>
 
-            <button type='button' onClick={(e) => {
-                e.preventDefault()
-            }} className='outline_btn'>
-              Sign Out
-            </button>
+
 
           </div>
         ) : (
-          <>
-            <ConnectButton label="Sign in">
-            </ConnectButton>      
-          </>
+          <></>
         )}
+        <ConnectButton>
+        </ConnectButton> 
       </div>
     </nav>
   )
