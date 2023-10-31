784626da16b71b86a18885c0a34d9344b711830e
Shaun1280 <jpanau@connect.ust.hk>
2023-10-31 11:59:04 +0800

    use axios for api

--- client/app/api/create/route.js
+++ client/app/api/create/route.js
@@ -1,21 +1,48 @@
-import sdk from 'api';
-const sdkInst = sdk('@getimg/v1.0#64l331elmooz4oo');
+import axios from 'axios';
 
 export const POST = async (request) => {
     const data = await request.json();
 
-    // If seed is empty, exclude it in the data
-    if (data.seed === undefined) {
-        delete data.seed;
-    }
+    // // If seed is empty, exclude it in the data
+    // if (data.seed === undefined) {
+    //     delete data.seed;
+    // }
 
     try {
-        await sdkInst.auth(process.env.SDK_AUTH);
+        const options = {
+            method: 'POST',
+            url: 'https://api.getimg.ai/v1/stable-diffusion/text-to-image',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                authorization: `Bearer ${process.env.SDK_AUTH}`
+            },
+            data: {
+                model: data.model,
+                prompt: `${data.prompt}`,
+                negative_prompt: `${data.negative_prompt}`,
+                width: data.width,
+                height: data.height,
+                steps: data.steps,
+                guidance: data.guidanceScale,
+                seed: data.seed,
+                scheduler: data.sampler
+            }
+        };
 
-        const { data } = await sdkInst.postStableDiffusionTextToImage(data);
+        console.log(options)
 
-        return new Response(JSON.stringify({ data }), { status: 200 })
+        // const res = await axios.request(options);
+        res = {}
+
+        console.log(res.data)
+
+        return new Response(
+            JSON.stringify(res.data),
+            { status: 200 }
+        )
     } catch (error) {
+        console.log(error)
         return new Response(JSON.stringify({
             message: 'Failed to create a new prompt',
             error: error
