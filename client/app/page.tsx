9898b1ef18bd4947e71bc68a32b74b0a8dc13e6f
Shane0821 <jpanav@connect.ust.hk>
2023-11-02 12:34:59 +0800

    redesign homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,60 +1,26 @@
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Arts</span>
-      </h1>
-      <p className="desc text-center">
-        Artopia is an open-source AI art platform for modern world to
-        discover, create and share creative arts
-      </p>
-      {/* <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Arts</span>
-      </h1>
-      <p className="desc text-center">
-        Artopia is an open-source AI art platform for modern world to
-        discover, create and share creative arts
-      </p>
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Arts</span>
-      </h1>
-      <p className="desc text-center">
-        Artopia is an open-source AI art platform for modern world to
-        discover, create and share creative arts
-      </p>
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Arts</span>
-      </h1>
-      <p className="desc text-center">
-        Artopia is an open-source AI art platform for modern world to
-        discover, create and share creative arts
-      </p>
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Arts</span>
-      </h1>
-      <p className="desc text-center">
-        Artopia is an open-source AI art platform for modern world to
-        discover, create and share creative arts
-      </p>
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Arts</span>
-      </h1>
-      <p className="desc text-center">
-        Artopia is an open-source AI art platform for modern world to
-        discover, create and share creative arts
-      </p> */}
+    <section className="w-full h-screen -mt-12 flex-between max-w-7xl">
+      <div className='z-10 -mt-20 pr-2'>
+        <h1 className="head_text text-center">
+          Discover & Share
+          <br />
+          <span className="orange_gradient text-center"> AI-Powered Arts</span>
+        </h1>
+        <p className="desc text-center">
+          Artopia is an open-source AI art platform for modern world to
+          discover, create and share creative arts
+        </p>
+      </div>
+      <div className="home_img_container">
+        <img src="/assets/images/museum.jpg" />
+        <img src="/assets/images/bone-king.png" />
+        <img src="/assets/images/ari.jpg" />
+        <img src="/assets/images/castle.png" />
+        <img src="/assets/images/blue-hair-girl.png" />
+        <img src="/assets/images/star-hall.png" />
+        <img src="/assets/images/sona.jpg" />
+      </div>
     </section>
   )
 }
