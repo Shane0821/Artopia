974017f54d08b4bea28c5cb0f2da4716181b8fa3
Shaun1280 <jpanau@connect.ust.hk>
2023-11-03 16:54:52 +0800

    update api route

--- client/app/api/create/route.js
+++ client/app/api/create/route.js
@@ -84,23 +84,3 @@ export const POST = async (request) => {
         }), { status: 500 });
     }
 }
-
-export const GET = async (request, { params }) => {
-
-    try {
-        await connectToDB()
-
-        const art = await Art.find({ address: params.address });
-
-        return new Response(
-            JSON.stringify({ art: art }),
-            { status: 200 }
-        )
-    } catch (error) {
-        console.log(error)
-        return new Response(JSON.stringify({
-            message: 'Failed to get',
-            error: error
-        }), { status: 500 });
-    }
-}
