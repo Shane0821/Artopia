ec6afb5c74a2a519cf75ef892e2232858c6184d0
Shane0821 <jpanav@connect.ust.hk>
2023-11-04 20:16:41 +0800

    add images

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   const centerImg = ['boy.png', 'boy.png', 'pumpkin.png', 'pumpkin-girl.png', 'eye.png', 'bone-king.png', 
                      'bone-king-2.png', 'bone-queen.png', 'jinx1.jpg', 'jinx2.jpg', 'christmas.png',
-                     'chrysta-bell.png', 'elegant-lady.png']
+                     'chrysta-bell.png', 'elegant-lady.png', 'assasin.png', 'transhumanism.png', 'transhumanism2.png']
   
   const getRandomCenterName = () => {
     var randomIndex = Math.floor(Math.random () * centerImg.length)
