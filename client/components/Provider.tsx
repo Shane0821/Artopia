cc981fb9f6e255c8cefbb6c07ae8c5cbfcdb6053
Shane0821 <jpanav@connect.ust.hk>
2023-11-04 20:06:53 +0800

    upd the list of chain

--- client/components/Provider.tsx
+++ client/components/Provider.tsx
@@ -16,18 +16,17 @@ import {
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
   mainnet,
+  sepolia,
   polygon,
   optimism,
   arbitrum,
-  base,
-  zora,
-  sepolia
+  base
 } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora, sepolia],
+  [mainnet, sepolia, polygon, optimism, arbitrum, base],
   [
     // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
