6765b1c72cba14c0a7e14cb6d6c167c98fee8e1c
Shane0821 <jpanav@connect.ust.hk>
2023-11-08 10:46:23 +0800

    upd prompt contract

--- contract/contracts/promptnft.sol
+++ contract/contracts/promptnft.sol
@@ -9,23 +9,30 @@ contract PromptNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping(string => uint8) cids;
+    mapping(string => uint256) cids;
 
     constructor() ERC721("PromptNFT", "PNFT") {}
 
     function awardItem(address user, string memory cid)
         public returns (uint256) {
-        require(cids[cid] != 1, "prompt already exists");
+        require(cids[cid] == 0, "prompt already exists");
 
-        cids[cid] = 1;
+        // increament to make sure that every element in cids > 0
+        _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
+        cids[cid] = newItemId;
+
         _mint(user, newItemId);
 
         string memory metadataURI = string.concat("ipfs://", cid);
         _setTokenURI(newItemId, metadataURI);
 
-        _tokenIds.increment();
         return newItemId;
+    }
+
+    function getOnwerByCID(string memory cid) public view returns (address) {
+        require(cids[cid] > 0, "prompt is not owned by anyone");
+        return ownerOf(cids[cid]);
     }
 }
