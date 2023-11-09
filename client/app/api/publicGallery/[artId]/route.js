c1289c2c914d8e553d1f4f8e87c2bb5260429929
Shaun1280 <jpanau@connect.ust.hk>
2023-11-09 09:08:18 +0800

    Update route.js

--- client/app/api/publicGallery/[artId]/route.js
+++ client/app/api/publicGallery/[artId]/route.js
@@ -3,36 +3,7 @@ import { getToken } from "next-auth/jwt"
 import Art from "@models/Art";
 import { connectToDB } from "@utils/database";
 
-export const DELETE = async (request, { params }) => {
-    try {
-        const token = await getToken({ req: request });
-        // Not Signed in
-        if (!token) throw new Error("Unauthorized.");
-        // get address
-        const address = token.sub;
-        const _id = params.artId;
-
-        // console.log(address, _id)
-
-        await connectToDB()
-        await Art.deleteOne({ _id, address });
-
-        return new Response(
-            JSON.stringify({}),
-            { status: 200 }
-        )
-    } catch (error) {
-        console.log(error)
-        return new Response(JSON.stringify({
-            message: 'Failed to create delete art.',
-            error: error
-        }), { status: 500 });
-    }
-}
-
-export const PATCH_SHARED = async (request, { params }) => {
-    const { title } = await request.json();
-
+export const PATCH = async (request, { params }) => {
     try {
         const token = await getToken({ req: request });
         // Not Signed in
