26e21f9ab00ea49be409807718154c2b4e593030
Shane0821 <jpanav@connect.ust.hk>
2023-11-12 13:14:35 +0800

    upd data format and fetch process

--- client/app/profile/[addr]/page.tsx
+++ client/app/profile/[addr]/page.tsx
@@ -6,13 +6,26 @@ import { useAccount } from "wagmi"
 import { useSession } from "next-auth/react"
 
 import { readContract } from '@wagmi/core'
-import imgABI from '/abi/imagenft.json'
-import promptABI from '/abi/promptnft.json'
-import { METHODS } from 'http';
+import imgABI from '@abi/imagenft.json'
+import promptABI from '@abi/promptnft.json'
+
 
 interface promptDataType {
     prompt: string,
     negative_prompt: string
+}
+
+interface artDataType {
+    name: string,
+    description: string,
+    cid: string,
+    promptcid: string,
+    model: string,
+    steps: number,
+    guidanceScale: number,
+    seed: number,
+    sampler: string,
+    createdAt: string
 }
 
 function page({ params }: { params: { addr: string } }) {
@@ -20,9 +33,9 @@ function page({ params }: { params: { addr: string } }) {
     const { address, isConnected } = useAccount()
 
     const [promptURIList, setPromptURIList] = useState<promptDataType[]>([])
-    const [artURIList, setArtURIList] = useState<string[]>([])
+    const [artList, setArtList] = useState<artDataType[]>([])
 
-    const imgIpfsOfOwnerByIndex = async (usr: string, idx: number) => {
+    const artOfOwnerByIndex = async (usr: string, idx: number) => {
         try {
             // get token id
             const tokenId: BigInt = await readContract({
@@ -44,7 +57,7 @@ function page({ params }: { params: { addr: string } }) {
             })
             const metaURI = 'https://ipfs.io/ipfs/' + tokenURI.split("ipfs://")[1]
 
-            // get ipfs uri
+            // get info from metadata
             const response = await fetch(metaURI, {
                 method: 'GET'
             })
@@ -53,7 +66,49 @@ function page({ params }: { params: { addr: string } }) {
                 throw new Error(message);
             }
             const data = await response.json();
-            return 'https://ipfs.io/ipfs/' + data.image
+
+            let imgData: artDataType = {
+                name: "",
+                description: "",
+                cid: "",
+                promptcid: "",
+                model: "",
+                steps: 0,
+                guidanceScale: 0,
+                seed: 0,
+                sampler: "",
+                createdAt: ""
+            };
+            imgData.name = data.name
+            imgData.description = data.description
+            imgData.cid = data.image.split("ipfs://")[0] // should be 1
+
+            data.attributes?.forEach((attribute: any) => {
+                switch (attribute.trait_type) {
+                    case 'Model':
+                        imgData.model = attribute.value;
+                        break;
+                    case 'Prompt':
+                        imgData.promptcid = attribute.value.split("ipfs://")[0]; // // should be 1
+                        break;
+                    case 'Steps':
+                        imgData.steps = attribute.value;
+                        break;
+                    case 'GuidanceScale':
+                        imgData.guidanceScale = attribute.value;
+                        break;
+                    case 'Seed':
+                        imgData.seed = attribute.value;
+                        break;
+                    case 'Sampler':
+                        imgData.sampler = attribute.value;
+                        break;
+                    case 'CreatedAt':
+                        imgData.createdAt = attribute.value;
+                        break;
+                }
+            })
+            return imgData
         } catch (error) {
             console.error(error);
             return undefined
@@ -103,56 +158,62 @@ function page({ params }: { params: { addr: string } }) {
 
         const loadPrompt = async (usr: string) => {
             if (!usr) return
+            try {
+                // get prompt nft count
+                const balance: BigInt = await readContract({
+                    address: process.env.NEXT_PUBLIC_PROMPT_NFT_CONTRACT,
+                    abi: promptABI,
+                    functionName: 'balanceOf',
+                    chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
+                    args: [usr]
+                })
+                const cntPrompt = Number(balance)
 
-            // get prompt nft count
-            const balance: BigInt = await readContract({
-                address: process.env.NEXT_PUBLIC_PROMPT_NFT_CONTRACT,
-                abi: promptABI,
-                functionName: 'balanceOf',
-                chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
-                args: [usr]
-            })
-            const cntPrompt = Number(balance)
-
-            // fetch all
-            var promptURIList: promptDataType[] = []
-            if (cntPrompt > 0) {
-                for await (const prompt of Array.from({ length: cntPrompt },
-                    (_, index) => promptOfOwnerByIndex(usr, index))) {
-                    if (prompt != undefined) {
-                        promptURIList.push(prompt)
+                // fetch all
+                var promptURIList: promptDataType[] = []
+                if (cntPrompt > 0) {
+                    for await (const prompt of Array.from({ length: cntPrompt },
+                        (_, index) => promptOfOwnerByIndex(usr, index))) {
+                        if (prompt != undefined) {
+                            promptURIList.push(prompt)
+                        }
                     }
                 }
+                console.log("prompt list", promptURIList)
+                setPromptURIList(promptURIList)
+            } catch (error) {
+                console.error(error);
             }
-            console.log("prompt uri list", promptURIList)
-            setPromptURIList(promptURIList)
         }
 
         const loadArt = async (usr: string) => {
             if (!usr) return
+            try {
+                // get art nft count
+                const balance: BigInt = await readContract({
+                    address: process.env.NEXT_PUBLIC_IMG_NFT_CONTRACT,
+                    abi: imgABI,
+                    functionName: 'balanceOf',
+                    chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
+                    args: [usr]
+                })
+                const cntArt = Number(balance)
 
-            // get art nft count
-            const balance: BigInt = await readContract({
-                address: process.env.NEXT_PUBLIC_IMG_NFT_CONTRACT,
-                abi: imgABI,
-                functionName: 'balanceOf',
-                chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
-                args: [usr]
-            })
-            const cntArt = Number(balance)
-            
-            // fetch all
-            var artURIList = []
-            if (cntArt > 0) {
-                for await (const ipfsURI of Array.from({ length: cntArt },
-                    (_, index) => imgIpfsOfOwnerByIndex(usr, index))) {
-                    if (ipfsURI != undefined) {
-                        artURIList.push(ipfsURI)
+                // fetch all
+                var artList = []
+                if (cntArt > 0) {
+                    for await (const ipfsURI of Array.from({ length: cntArt },
+                        (_, index) => artOfOwnerByIndex(usr, index))) {
+                        if (ipfsURI != undefined) {
+                            artList.push(ipfsURI)
+                        }
                     }
                 }
+                console.log("art uri list", artList)
+                setArtList(artList)
+            } catch (error) {
+                console.error(error);
             }
-            console.log("art uri list", artURIList)
-            setArtURIList(artURIList)
         }
 
         loadPrompt(params.addr);
