8517200792f2eca277390003845e01adbf9b9760
Shane0821 <jpanav@connect.ust.hk>
2023-11-12 17:25:18 +0800

    4 spaces -> 2 spaces

--- client/app/profile/[addr]/page.tsx
+++ client/app/profile/[addr]/page.tsx
@@ -10,208 +10,214 @@ import imgABI from '@abi/imagenft.json'
 import promptABI from '@abi/promptnft.json'
 
 import { 
-    getPromptCountByUser,
-    getPromptTokenIdOfUserByIndex,
-    getTokenURIOfPromptByTokenId,
-    getArtCountByUser,
-    getArtTokenIdOfUserByIndex,
-    getTokenURIOfArtByTokenId
+  getPromptCountByUser,
+  getPromptTokenIdOfUserByIndex,
+  getTokenURIOfPromptByTokenId,
+  getArtCountByUser,
+  getArtTokenIdOfUserByIndex,
+  getTokenURIOfArtByTokenId
 } from '@utils/contract'
 
 interface promptDataType {
-    prompt: string,
-    negative_prompt: string
+  prompt: string,
+  negative_prompt: string
 }
 
 interface artDataType {
-    name: string,
-    description: string,
-    cid: string,
-    promptcid: string,
-    model: string,
-    steps: number,
-    guidanceScale: number,
-    seed: number,
-    sampler: string,
-    createdAt: string
+  name: string,
+  description: string,
+  cid: string,
+  promptcid: string,
+  model: string,
+  steps: number,
+  guidanceScale: number,
+  seed: number,
+  sampler: string,
+  createdAt: string
 }
 
 function page({ params }: { params: { addr: string } }) {
-    const { data: session, status } = useSession()
-    const { address, isConnected } = useAccount()
+  const { data: session, status } = useSession()
+  const { address, isConnected } = useAccount()
 
-    const [promptURIList, setPromptURIList] = useState<promptDataType[]>([])
-    const [artList, setArtList] = useState<artDataType[]>([])
+  const [promptURIList, setPromptURIList] = useState<promptDataType[]>([])
+  const [artList, setArtList] = useState<artDataType[]>([])
 
-    const artOfOwnerByIndex = async (usr: string, idx: number) => {
-        try {
-            // get token id
-            const artId = await getArtTokenIdOfUserByIndex(usr, idx)
+  const artOfOwnerByIndex = async (usr: string, idx: number) => {
+    try {
+      // get token id
+      const artId = await getArtTokenIdOfUserByIndex(usr, idx)
 
-            // get metadata uri
-            const tokenURI: string = await getTokenURIOfArtByTokenId(artId)
-            const metaURI = 'https://ipfs.io/ipfs/' + tokenURI.split("ipfs://")[1]
+      // get metadata uri
+      const tokenURI: string = await getTokenURIOfArtByTokenId(artId)
+      const metaURI = 'https://ipfs.io/ipfs/' + tokenURI.split("ipfs://")[1]
 
-            // get info from metadata
-            const response = await fetch(metaURI, {
-                method: 'GET'
-            })
-            if (!response.ok) {
-                const message = `An error has occurred: ${response.status}`;
-                throw new Error(message);
-            }
-            const data = await response.json();
+      // get info from metadata
+      const response = await fetch(metaURI, {
+        method: 'GET'
+      })
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.status}`;
+        throw new Error(message);
+      }
+      const data = await response.json();
 
-            let imgData: artDataType = {
-                name: "",
-                description: "",
-                cid: "",
-                promptcid: "",
-                model: "",
-                steps: 0,
-                guidanceScale: 0,
-                seed: 0,
-                sampler: "",
-                createdAt: ""
-            };
-            imgData.name = data.name
-            imgData.description = data.description
-            imgData.cid = data.image.split("ipfs://")[0] // should be 1
+      let imgData: artDataType = {
+        name: "",
+        description: "",
+        cid: "",
+        promptcid: "",
+        model: "",
+        steps: 0,
+        guidanceScale: 0,
+        seed: 0,
+        sampler: "",
+        createdAt: ""
+      };
+      imgData.name = data.name
+      imgData.description = data.description
+      imgData.cid = data.image.split("ipfs://")[0] // should be 1
 
-            data.attributes?.forEach((attribute: any) => {
-                switch (attribute.trait_type) {
-                    case 'Model':
-                        imgData.model = attribute.value;
-                        break;
-                    case 'Prompt':
-                        imgData.promptcid = attribute.value.split("ipfs://")[0]; // // should be 1
-                        break;
-                    case 'Steps':
-                        imgData.steps = attribute.value;
-                        break;
-                    case 'GuidanceScale':
-                        imgData.guidanceScale = attribute.value;
-                        break;
-                    case 'Seed':
-                        imgData.seed = attribute.value;
-                        break;
-                    case 'Sampler':
-                        imgData.sampler = attribute.value;
-                        break;
-                    case 'CreatedAt':
-                        imgData.createdAt = attribute.value;
-                        break;
-                }
-            })
-            return imgData
-        } catch (error) {
-            console.error(error);
-            return undefined
+      data.attributes?.forEach((attribute: any) => {
+        switch (attribute.trait_type) {
+          case 'Model':
+            imgData.model = attribute.value;
+            break;
+          case 'Prompt':
+            imgData.promptcid = attribute.value.split("ipfs://")[0]; // // should be 1
+            break;
+          case 'Steps':
+            imgData.steps = attribute.value;
+            break;
+          case 'GuidanceScale':
+            imgData.guidanceScale = attribute.value;
+            break;
+          case 'Seed':
+            imgData.seed = attribute.value;
+            break;
+          case 'Sampler':
+            imgData.sampler = attribute.value;
+            break;
+          case 'CreatedAt':
+            imgData.createdAt = attribute.value;
+            break;
         }
+      })
+      return imgData
+    } catch (error) {
+      console.error(error);
+      return undefined
+    }
+  }
+
+  const promptOfOwnerByIndex = async (usr: string, idx: number) => {
+    try {
+      // get token id
+      const promptId = await getPromptTokenIdOfUserByIndex(usr, idx)
+
+      // get metadata uri
+      const tokenURI: string = await getTokenURIOfPromptByTokenId(promptId)
+      const metaURI = 'https://ipfs.io/ipfs/' + tokenURI.split("ipfs://")[1]
+      
+      // get prompt from metadata
+      const response = await fetch(metaURI, {
+        method: 'GET'
+      })
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.status}`;
+        throw new Error(message);
+      }
+      const data = await response.json();
+
+      return data.textData
+    } catch (error) {
+      console.error(error);
+      return undefined
+    }
+  }
+
+  useEffect(() => {
+    if (!isConnected) return
+
+    const loadPrompt = async (usr: string) => {
+      if (!usr) return
+      try {
+        // get prompt nft count
+        const cntPrompt = await getPromptCountByUser(usr)
+
+        // fetch all
+        var promptURIList: promptDataType[] = []
+        if (cntPrompt > 0) {
+          for await (const prompt of Array.from({ length: cntPrompt },
+            (_, index) => promptOfOwnerByIndex(usr, index))) {
+            if (prompt != undefined) {
+              promptURIList.push(prompt)
+            }
+          }
+        }
+        console.log("prompt list", promptURIList)
+        setPromptURIList(promptURIList.reverse())
+      } catch (error) {
+        console.error(error);
+      }
     }
 
-    const promptOfOwnerByIndex = async (usr: string, idx: number) => {
-        try {
-            // get token id
-            const promptId = await getPromptTokenIdOfUserByIndex(usr, idx)
+    const loadArt = async (usr: string) => {
+      if (!usr) return
+      try {
+        // get art nft count
+        const cntArt = await getArtCountByUser(usr)
 
-            // get metadata uri
-            const tokenURI: string = await getTokenURIOfPromptByTokenId(promptId)
-            const metaURI = 'https://ipfs.io/ipfs/' + tokenURI.split("ipfs://")[1]
-            
-            // get prompt from metadata
-            const response = await fetch(metaURI, {
-                method: 'GET'
-            })
-            if (!response.ok) {
-                const message = `An error has occurred: ${response.status}`;
-                throw new Error(message);
+        // fetch all
+        var artList = []
+        if (cntArt > 0) {
+          for await (const ipfsURI of Array.from({ length: cntArt },
+            (_, index) => artOfOwnerByIndex(usr, index))) {
+            if (ipfsURI != undefined) {
+              artList.push(ipfsURI)
             }
-            const data = await response.json();
-
-            return data.textData
-        } catch (error) {
-            console.error(error);
-            return undefined
+          }
         }
+        console.log("art uri list", artList)
+        setArtList(artList.reverse())
+      } catch (error) {
+        console.error(error);
+      }
     }
 
-    useEffect(() => {
-        if (!isConnected) return
+    loadPrompt(params.addr);
+    loadArt(params.addr)
+  }, [isConnected]);
 
-        const loadPrompt = async (usr: string) => {
-            if (!usr) return
-            try {
-                // get prompt nft count
-                const cntPrompt = await getPromptCountByUser(usr)
+  return (
+    <section className="w-full flex-center flex-col">
+      <div className='mt-12 text-center'>
+        <h2 className='font-display text-4xl font-extrabold leading-tight text-black sm:text-5xl sm:leading-tight'>
+          {"Your "} 
+          <span className="bg-gradient-to-r from-red-600 to-amber-600 bg-clip-text text-transparent">
+            {"Art"}
+          </span>
+        </h2>
+        <p className="mt-5 text-gray-600 sm:text-lg">Express, inspire, connect - a collection of creative talents.</p>
 
-                // fetch all
-                var promptURIList: promptDataType[] = []
-                if (cntPrompt > 0) {
-                    for await (const prompt of Array.from({ length: cntPrompt },
-                        (_, index) => promptOfOwnerByIndex(usr, index))) {
-                        if (prompt != undefined) {
-                            promptURIList.push(prompt)
-                        }
-                    }
-                }
-                console.log("prompt list", promptURIList)
-                setPromptURIList(promptURIList)
-            } catch (error) {
-                console.error(error);
-            }
-        }
+      </div>
+      {/* list of art */}
+      
+      
 
-        const loadArt = async (usr: string) => {
-            if (!usr) return
-            try {
-                // get art nft count
-                const cntArt = await getArtCountByUser(usr)
 
-                // fetch all
-                var artList = []
-                if (cntArt > 0) {
-                    for await (const ipfsURI of Array.from({ length: cntArt },
-                        (_, index) => artOfOwnerByIndex(usr, index))) {
-                        if (ipfsURI != undefined) {
-                            artList.push(ipfsURI)
-                        }
-                    }
-                }
-                console.log("art uri list", artList)
-                setArtList(artList)
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        loadPrompt(params.addr);
-        loadArt(params.addr)
-    }, [isConnected]);
-
-    return (
-        <section className="w-full flex-center flex-col">
-            <div className='mt-12 text-center'>
-                <h2 className='font-display text-4xl font-extrabold leading-tight text-black sm:text-5xl sm:leading-tight'>
-                    {"Your "} 
-                    <span className="bg-gradient-to-r from-red-600 to-amber-600 bg-clip-text text-transparent">
-                        {"Art"}
-                    </span>
-                </h2>
-                <p className="mt-5 text-gray-600 sm:text-lg">Express, inspire, connect - a collection of creative talents.</p>
-            </div>
-
-            <div className='mt-12 text-center'>
-                <h2 className='font-display text-4xl font-extrabold leading-tight text-black sm:text-5xl sm:leading-tight'>
-                    {"Your "} 
-                    <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
-                        {"Prompts"}
-                    </span>
-                </h2>
-                <p className="mt-5 text-gray-600 sm:text-lg">Spark your imagination and creativity - unleash your potential and passion.</p>
-            </div>
-        </section>
-    )
+      <div className='mt-12 text-center'>
+        <h2 className='font-display text-4xl font-extrabold leading-tight text-black sm:text-5xl sm:leading-tight'>
+          {"Your "} 
+          <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+            {"Prompts"}
+          </span>
+        </h2>
+        <p className="mt-5 text-gray-600 sm:text-lg">Spark your imagination and creativity - unleash your potential and passion.</p>
+      </div>
+      {/* list of prompts */}
+    </section>
+  )
 }
 
 export default page
